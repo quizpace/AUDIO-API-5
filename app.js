0569d8225ffa1da637763c2c5e6b76a242da6f4a
Avi <drazninavi@gmail.com>
2023-11-20 15:50:52 +0200

    a

--- app.js
+++ app.js
@@ -1,52 +1,3 @@
-const express = require("express");
-const multer = require("multer");
-const path = require("path");
-
-const app = express();
-const port = process.env.PORT || 2000; // Use the environment port if available
-
-// Multer configuration for file upload
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/"); // Updated path to refer to the 'uploads' folder in the main project directory
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-// Endpoint for starting recording (simulated)
-app.post("/start-recording", (req, res) => {
-  // Logic to start recording (if using a frontend, this will trigger recording on the client-side)
-  res.status(200).send("Recording started");
-});
-
-// Endpoint for stopping recording and uploading
-app.post("/stop-recording-upload", upload.single("audio"), (req, res) => {
-  // Logic to stop recording and handle the uploaded file
-  if (!req.file) {
-    return res.status(400).send("No file uploaded");
-  }
-  // Process the uploaded file, you can save it to a database or perform other actions
-  res.status(200).send("Recording stopped and file uploaded successfully");
-});
-
-// Define the root path handler
-app.get("/", (req, res) => {
-  res.send("Welcome to the audio recording API");
-});
-
-// Serve uploaded files
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-//--------
-
 // const express = require("express");
 // const multer = require("multer");
 // const path = require("path");
@@ -62,17 +13,6 @@ app.listen(port, () => {
 //   filename: function (req, file, cb) {
 //     cb(null, file.originalname);
 //   },
-// });
-
-// // Enable CORS - Allow all origins
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
 // });
 
 // const upload = multer({ storage: storage });
@@ -104,6 +44,66 @@ app.listen(port, () => {
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
 // });
+
+//--------
+
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 2000; // Use the environment port if available
+
+// Multer configuration for file upload
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/"); // Updated path to refer to the 'uploads' folder in the main project directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+// Enable CORS - Allow all origins
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+const upload = multer({ storage: storage });
+
+// Endpoint for starting recording (simulated)
+app.post("/start-recording", (req, res) => {
+  // Logic to start recording (if using a frontend, this will trigger recording on the client-side)
+  res.status(200).send("Recording started");
+});
+
+// Endpoint for stopping recording and uploading
+app.post("/stop-recording-upload", upload.single("audio"), (req, res) => {
+  // Logic to stop recording and handle the uploaded file
+  if (!req.file) {
+    return res.status(400).send("No file uploaded");
+  }
+  // Process the uploaded file, you can save it to a database or perform other actions
+  res.status(200).send("Recording stopped and file uploaded successfully");
+});
+
+// Define the root path handler
+app.get("/", (req, res) => {
+  res.send("Welcome to the audio recording API");
+});
+
+// Serve uploaded files
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 //------
 
